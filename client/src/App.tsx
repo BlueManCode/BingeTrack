5cb7a85069a39f2189791693fbefa0e88bc0d025
bluemancode <amanssingh2307@gmail.com>
2022-07-11 14:13:52 -0400

    added react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
+// Public Pages
+import LoginPage from './pages/Public/LoginPage';
+import SignUpPage from './pages/Public/SignUpPage';
+
+// Private Pages
+import DashBoardPage from './pages/Private/DashBoardPage';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/login' element={<LoginPage />}></Route>
+      <Route path='/signup' element={<SignUpPage />}></Route>
+      <Route path='/' element={<DashBoardPage />}></Route>
+    </Routes>
   );
-}
+};
 
 export default App;
